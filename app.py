a769bc956d883e545e62df6ba4c7ef4b28d0dc5f
Paul MacKay <paul.mackay.a@gmail.com>
2021-03-23 22:04:20 -0400

    try another db driver

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-import pymssql  
+import pyodbc
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    conn = pymssql.connect(server='cloudthingsdb.database.windows.net', user='cloudthingsadmin', password='MVCM7qs7BQ62Ci4', database='cloudthings')
+    conn = pyodbc.connect(server='cloudthingsdb.database.windows.net', user='cloudthingsadmin', password='MVCM7qs7BQ62Ci4', database='cloudthings', driver= '{ODBC Driver 17 for SQL Server}')
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+

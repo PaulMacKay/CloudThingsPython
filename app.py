905efebd97a0f08e9b87153ed231a2afc6a21052
Paul MacKay <paul.mackay.a@gmail.com>
2021-03-29 10:40:20 -0400

    Removed mysql import

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-import pymssql
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    conn = pymsspl.connect(server='cloudthingsdb.database.windows.net', user='cloudthingsadmin', password='MVCM7qs7BQ62Ci4', database='cloudthings')
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    print(posts)
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
 
 
